a02e8b69f9b7e1c7617e1f61467467ce1a67e598
Edwige ROUSSIN <edwige.roussin@gmail.com>
2024-10-15 12:20:30 +0200

    add gitignore

---
+++ script.js
@@ -0,0 +1,13 @@
+import chalk from 'chalk';
+
+const prenom1 ='Tiago';
+const prenom2='Emilie';
+const prenom3 ='Edwige';
+const prenom4 ='Sidi';
+
+ 
+console.log(chalk.green(prenom1));
+console.log(chalk.red (prenom2));
+console.log(chalk.yellow(prenom3));
+console.log(chalk.bgGray(prenom4));
+
